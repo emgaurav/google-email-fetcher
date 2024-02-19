b4a0ada6c7cb42160c89ae0b4f7f7b131013bec1
Gaurav Arora <38912972+emgaurav@users.noreply.github.com>
2024-02-20 00:58:16 +0530

    Update script.py

--- script.py
+++ script.py
@@ -5,31 +5,39 @@ from oauth2client.service_account import ServiceAccountCredentials
 # Configuration 
 SCOPES = ['https://www.googleapis.com/auth/admin.directory.user.readonly'] 
 SERVICE_ACCOUNT_FILE = '</path/to/your_json_file>' 
+MAX_RESULTS_PER_REQUEST = 500  # Adjust as needed
 
 # Authentication
 credentials = ServiceAccountCredentials.from_json_keyfile_name(
     SERVICE_ACCOUNT_FILE, scopes=SCOPES)
-delegated_credentials = credentials.create_delegated('admin@domain.com') # Admin user email with necessary permissions
+delegated_credentials = credentials.create_delegated('admin@domain.com') # Admin with necessary permissions
 http = delegated_credentials.authorize(httplib2.Http())
 service = discovery.build('admin', 'directory_v1', http=http) 
 
 # Fetching Active Users and Emails
-def fetch_all_users():
-    email_list = []
+def fetch_users_chunk(page_token=None):
     request = service.users().list(customer='my_customer', 
                                    query="isSuspended=false", 
-                                   fields='nextPageToken,users(primaryEmail)')
-    count = 0 
+                                   fields='nextPageToken,users(primaryEmail)',
+                                   maxResults=MAX_RESULTS_PER_REQUEST,
+                                   pageToken=page_token)
+    users_result = request.execute()
+    users = users_result.get('users', [])
+    email_chunk = [user['primaryEmail'] for user in users]
+    next_page_token = users_result.get('nextPageToken')
+    return email_chunk, next_page_token
 
-    while request is not None:
-        users_result = request.execute()
+def fetch_all_users():
+    email_list = []
+    next_page_token = None
 
-        users = users_result.get('users', [])
-        email_list.extend([user['primaryEmail'] for user in users])
-        count += len(users) 
-        print(f"Fetched {count} users so far...")
+    while True:
+        email_chunk, next_page_token = fetch_users_chunk(next_page_token)
+        email_list.extend(email_chunk)
+        print(f"Fetched {len(email_list)} users so far...")
 
-        request = service.users().list_next(request, users_result)
+        if not next_page_token:
+            break
 
     # Writing to the text file
     with open('active_user_emails.txt', 'w') as output_file:
@@ -37,4 +45,4 @@ def fetch_all_users():
             output_file.write(email + '\n')
 
 if __name__ == '__main__':
-    fetch_all_users()  # No need to fetch and store all_emails anymore
+    fetch_all_users()
